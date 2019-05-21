05a09e03a48e1138cef2365e2c400f0e308a8a8a
Matthew Pham <matthew.pham@cgs.act.edu.au>
2019-05-21 17:08:06 +1000

    Updated bug with base to add a “/“ at the end

--- root-path.js
+++ root-path.js
@@ -3,7 +3,7 @@ base.setAttribute("href", rootPath());
 document.head.appendChild(base);
 
 function rootPath() {
-  var path=document.URL.substring(0,document.URL.indexOf("/", 30));
+  var path=document.URL.substring(0,document.URL.indexOf("/", 30) + "/");
   console.log(path);
   return path;
 }
