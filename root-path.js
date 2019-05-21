da260ee436c71935d578c61660ccfd0803ae7902
Matthew Pham <matthew.pham@cgs.act.edu.au>
2019-05-21 16:29:10 +1000

    Added js to set base to root path in ED STEM

---
+++ root-path.js
@@ -0,0 +1,9 @@
+var base = document.createElement("base");
+base.setAttribute("href", rootPath());
+document.head.appendChild(base);
+
+function rootPath() {
+  var path=document.URL.substring(0,document.URL.indexOf("/", 30));
+  console.log(path);
+  return path;
+}
